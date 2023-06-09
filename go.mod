5c3b25231a452709fe925321ccd82a2b75764c0a
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2023-06-09 12:31:07 +0900

    first import

---
+++ go.mod
@@ -0,0 +1,43 @@
+module github.com/mattn/nostr-mahjongbot
+
+go 1.20
+
+require (
+	github.com/labstack/echo/v4 v4.10.2
+	github.com/lib/pq v1.10.9
+	github.com/nbd-wtf/go-nostr v0.18.8
+	github.com/nfnt/resize v0.0.0-20180221191011-83c6a9932646
+	github.com/uptrace/bun v1.1.14
+	github.com/uptrace/bun/dialect/pgdialect v1.1.14
+)
+
+require (
+	github.com/btcsuite/btcd/btcec/v2 v2.2.0 // indirect
+	github.com/btcsuite/btcd/btcutil v1.1.3 // indirect
+	github.com/btcsuite/btcd/chaincfg/chainhash v1.0.1 // indirect
+	github.com/decred/dcrd/crypto/blake256 v1.0.0 // indirect
+	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.0.1 // indirect
+	github.com/gobwas/httphead v0.1.0 // indirect
+	github.com/gobwas/pool v0.2.1 // indirect
+	github.com/gobwas/ws v1.2.0 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/josharian/intern v1.0.0 // indirect
+	github.com/labstack/gommon v0.4.0 // indirect
+	github.com/mailru/easyjson v0.7.7 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.19 // indirect
+	github.com/puzpuzpuz/xsync v1.5.2 // indirect
+	github.com/tidwall/gjson v1.14.4 // indirect
+	github.com/tidwall/match v1.1.1 // indirect
+	github.com/tidwall/pretty v1.2.0 // indirect
+	github.com/tmthrgd/go-hex v0.0.0-20190904060850-447a3041c3bc // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasttemplate v1.2.2 // indirect
+	github.com/vmihailenco/msgpack/v5 v5.3.5 // indirect
+	github.com/vmihailenco/tagparser/v2 v2.0.0 // indirect
+	golang.org/x/crypto v0.6.0 // indirect
+	golang.org/x/exp v0.0.0-20221106115401-f9659909a136 // indirect
+	golang.org/x/net v0.7.0 // indirect
+	golang.org/x/sys v0.8.0 // indirect
+	golang.org/x/text v0.7.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,378 @@
+package main
+
+import (
+	"bytes"
+	"context"
+	"crypto/sha256"
+	"database/sql"
+	"embed"
+	"encoding/hex"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"image"
+	"image/draw"
+	"image/png"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"regexp"
+	"sort"
+	"strconv"
+	"time"
+
+	"math/rand"
+
+	"github.com/labstack/echo/v4"
+	_ "github.com/lib/pq"
+	"github.com/nbd-wtf/go-nostr"
+	"github.com/nbd-wtf/go-nostr/nip19"
+	"github.com/uptrace/bun"
+	"github.com/uptrace/bun/dialect/pgdialect"
+)
+
+var (
+	cmdStart = regexp.MustCompile(` start$`)
+	cmdDrop  = regexp.MustCompile(` drop [0-9]$`)
+	cmdJudge = regexp.MustCompile(` judge$`)
+
+	//go:embed static
+	assets embed.FS
+)
+
+func judge2(ctx Result) bool {
+	if len(ctx.mentu) == 12 {
+		return true
+	}
+	for n := 0; n < len(ctx.hai); n++ {
+		if ctx.hai[n] >= 3 {
+			ctx.mentu = append(ctx.mentu, n, n, n)
+			ctx.hai[n] -= 3
+			if judge2(ctx) {
+				return true
+			}
+		}
+	}
+	for n := 0; n < len(ctx.hai)-2; n++ {
+		if ctx.hai[n] > 0 && ctx.hai[n+1] > 0 && ctx.hai[n+2] > 0 {
+			ctx.mentu = append(ctx.mentu, n+0, n+1, n+2)
+			ctx.hai[n+0] -= 1
+			ctx.hai[n+1] -= 1
+			ctx.hai[n+2] -= 1
+			if judge2(ctx) {
+				return true
+			}
+		}
+	}
+	return false
+}
+
+type Result struct {
+	hai   []int
+	mentu []int
+	atama int
+}
+
+func judge1(hai []int) Result {
+	ctx := Result{
+		hai:   hai,
+		mentu: []int{},
+		atama: -1,
+	}
+	for n := 0; n < len(hai); n++ {
+		if hai[n] >= 2 {
+			nhai := make([]int, len(hai))
+			copy(nhai, hai)
+			ctx = Result{hai: nhai, mentu: []int{}, atama: n}
+			ctx.hai[n] -= 2
+			if judge2(ctx) {
+				break
+			}
+		}
+		ctx.atama = -1
+	}
+	return ctx
+}
+
+func judge(chai []int) *Result {
+	hai := []int{0, 0, 0, 0, 0, 0, 0, 0, 0}
+	for n := 0; n < len(chai); n++ {
+		hai[chai[n]] += 1
+	}
+	ret := judge1(hai)
+	if ret.atama == -1 {
+		return nil
+	}
+	return &ret
+}
+
+func (g *game) drop(v int) bool {
+	for i := 0; i < len(g.Data.Hai); i++ {
+		if g.Data.Hai[i] == v {
+			copy(g.Data.Hai[i:], g.Data.Hai[i+1:])
+			g.Data.Hai = g.Data.Hai[:len(g.Data.Hai)-1]
+			return true
+		}
+	}
+	return false
+}
+
+func (g *game) take() int {
+	rest := 0
+	for _, v := range g.Data.Mountain {
+		rest += v
+	}
+	if rest == 0 {
+		return -1
+	}
+	for {
+		n := rand.Int() % 9
+		if g.Data.Mountain[n] > 0 {
+			g.Data.Mountain[n] -= 1
+			g.Data.Hai = append(g.Data.Hai, n)
+			sort.Ints(g.Data.Hai)
+			return n
+		}
+	}
+}
+
+/*
+let mountain = [4, 4, 4, 4, 4, 4, 4, 4, 4]
+let hai = []
+for (let n = 0; n < 13; n++) hai.push(take(mountain))
+hai = hai.sort()
+let t = take(mountain)
+hai.push(t)
+hai = hai.sort()
+let ret = judge(hai)
+if (ret != null) {
+  //console.log(hai)
+  console.log(JSON.stringify(hai))
+  console.log(ret.mentu)
+  console.log(ret.atama)
+}
+*/
+
+type Data struct {
+	Mountain []int `json:"mountain"`
+	Hai      []int `json:"hai"`
+}
+
+type game struct {
+	bun.BaseModel `bun:"table:mahjong_game,alias:g"`
+	ID            string    `bun:"id,pk,notnull" json:"id"`
+	Npub          string    `bun:"npub,notnull" json:"npub"`
+	Data          Data      `bun:"data,type:jsonb" json:"data"`
+	Ref           string    `bun:"ref,notnull" json:"ref"`
+	CreatedAt     time.Time `bun:"created_at,notnull,default:current_timestamp" json:"created_at"`
+}
+
+func upload(buf *bytes.Buffer) (string, error) {
+	req, err := http.NewRequest(http.MethodPost, "https://void.cat/upload?cli=true", buf)
+	if err != nil {
+		return "", err
+	}
+	req.Header.Set("V-Content-Type", "image/png")
+	result := sha256.Sum256(buf.Bytes())
+	req.Header.Set("V-Full-Digest", hex.EncodeToString(result[:]))
+	req.Header.Set("V-Filename", "image.png")
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return "", err
+	}
+	defer req.Body.Close()
+
+	b, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+	return string(b), nil
+}
+
+func makeImage(hai []int) (string, error) {
+	bounds := image.Rect(0, 0, (128+2)*14+3+4, 183+4)
+
+	dst := image.NewRGBA(bounds)
+
+	for i, v := range hai {
+		f, err := assets.Open(fmt.Sprintf("static/mahjong-p%d.png", v+1))
+		if err != nil {
+			return "", err
+		}
+		defer f.Close()
+		emoji, _, err := image.Decode(f)
+		if err != nil {
+			return "", err
+		}
+		off := 0
+		if i == 13 {
+			off = 2
+		}
+		draw.Draw(
+			dst,
+			image.Rect(
+				i*(emoji.Bounds().Dx()+2)+off+2,
+				2,
+				(i+1)*(emoji.Bounds().Dx()+2)+off,
+				emoji.Bounds().Dy(),
+			),
+			emoji, image.ZP,
+			draw.Over)
+	}
+
+	var buf bytes.Buffer
+	err := png.Encode(&buf, dst)
+	if err != nil {
+		return "", err
+	}
+	return upload(&buf)
+}
+
+func main() {
+	db, err := sql.Open("postgres", os.Getenv("DATABASE_URL"))
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	bundb := bun.NewDB(db, pgdialect.New())
+	defer bundb.Close()
+
+	_, err = bundb.NewCreateTable().Model((*game)(nil)).IfNotExists().Exec(context.Background())
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	var sk, pub string
+	if _, s, err := nip19.Decode(os.Getenv("BOT_NSEC")); err != nil {
+		log.Fatal(err)
+	} else {
+		sk = s.(string)
+	}
+	if pub, err = nostr.GetPublicKey(sk); err == nil {
+		if _, err := nip19.EncodePublicKey(pub); err != nil {
+			log.Fatal(err)
+		}
+	} else {
+		log.Fatal(err)
+	}
+
+	e := echo.New()
+	e.POST("/api", func(c echo.Context) error {
+		ev := nostr.Event{}
+		err := json.NewDecoder(c.Request().Body).Decode(&ev)
+		if err != nil {
+			log.Println(err)
+			return c.JSON(http.StatusInternalServerError, err.Error())
+		}
+
+		var g game
+		etag := ev.Tags.GetFirst([]string{"e"})
+		if etag == nil && cmdStart.MatchString(ev.Content) {
+			ev.PubKey = pub
+			ev.Tags = ev.Tags.AppendUnique(nostr.Tag{"e", ev.ID})
+			ev.CreatedAt = nostr.Now()
+			ev.Kind = nostr.KindTextNote
+
+			g.ID = ev.ID
+			g.Npub = ev.PubKey
+			g.Data.Mountain = []int{4, 4, 4, 4, 4, 4, 4, 4, 4}
+			for n := 0; n < 14; n++ {
+				g.take()
+			}
+			_, err = bundb.NewInsert().Model(&g).Exec(context.Background())
+			if err != nil {
+				log.Println("fail insert", err)
+				return c.JSON(http.StatusInternalServerError, err.Error())
+			}
+			img, err := makeImage(g.Data.Hai)
+			if err != nil {
+				log.Println("fail makeImage", err)
+				return c.JSON(http.StatusInternalServerError, err.Error())
+			}
+			ev.Content = img
+			if err := ev.Sign(sk); err != nil {
+				log.Println(err)
+				log.Println("fail sign", err)
+				return c.JSON(http.StatusInternalServerError, err.Error())
+			}
+			return c.JSON(http.StatusOK, ev)
+		} else if etag != nil && cmdDrop.MatchString(ev.Content) {
+			ev.PubKey = pub
+			ev.Tags = ev.Tags.AppendUnique(nostr.Tag{"e", ev.ID})
+			ev.CreatedAt = nostr.Now()
+			ev.Kind = nostr.KindTextNote
+
+			v, err := strconv.Atoi(ev.Content[len(ev.Content)-2:])
+			if err != nil {
+				ev.Content = "不正な番号です"
+				if err := ev.Sign(sk); err != nil {
+					log.Println(err)
+					return c.JSON(http.StatusInternalServerError, err.Error())
+				}
+				return c.JSON(http.StatusOK, ev)
+			}
+
+			err = bundb.NewSelect().Model((*game)(nil)).Where("ID = ?", etag.Value()).Scan(context.Background(), &g)
+			if err != nil {
+				ev.Content = "不正な参照です"
+				if err := ev.Sign(sk); err != nil {
+					log.Println(err)
+					return c.JSON(http.StatusInternalServerError, err.Error())
+				}
+				return c.JSON(http.StatusOK, ev)
+			}
+
+			if !g.drop(v) {
+				err = errors.New("bad")
+				log.Println(err)
+				return c.JSON(http.StatusInternalServerError, err.Error())
+				ev.Content = "牌を捨てる事ができません"
+				if err := ev.Sign(sk); err != nil {
+					log.Println(err)
+					return c.JSON(http.StatusInternalServerError, err.Error())
+				}
+				return c.JSON(http.StatusOK, ev)
+			}
+			g.take()
+
+			tx, err := bundb.Begin()
+			if err != nil {
+				log.Println(err)
+				return c.JSON(http.StatusInternalServerError, err.Error())
+			}
+			defer tx.Rollback()
+
+			_, err = tx.NewDelete().Model((*game)(nil)).Where("ID = ?", g.Ref).Exec(context.Background())
+			if err != nil {
+				log.Println(err)
+				return c.JSON(http.StatusInternalServerError, err.Error())
+			}
+			g.Ref = ev.ID
+
+			_, err = tx.NewInsert().Model(&g).Exec(context.Background())
+			if err != nil {
+				log.Println(err)
+				return c.JSON(http.StatusInternalServerError, err.Error())
+			}
+			tx.Commit()
+
+			img, err := makeImage(g.Data.Hai)
+			if err != nil {
+				log.Println(err)
+				return c.JSON(http.StatusInternalServerError, err.Error())
+			}
+			ev.Content = img
+			if err := ev.Sign(sk); err != nil {
+				log.Println(err)
+				return c.JSON(http.StatusInternalServerError, err.Error())
+			}
+			return c.JSON(http.StatusOK, ev)
+		}
+
+		return c.JSON(http.StatusOK, "")
+	})
+	e.Logger.Fatal(e.Start(":8989"))
+}
